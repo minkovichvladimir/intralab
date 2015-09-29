3535d07be7319f54e1be4088f00c9e54c4b32393
Минькович Владимир <elit_kislot@hotmail.com>
2015-09-29 11:55:19 +0300

    Разделение услуг по персональным страницам

---
+++ intralab/Controllers/ServicesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace intralab.Controllers
+{
+    public class ServicesController : Controller
+    {   
+        //Беспроводные сети
+        public ActionResult Wireless()
+        {
+            return View();
+        }
+
+        //Подбор, приобретение, запуск в эксплуатацию оборудования
+        public ActionResult Equipment()
+        {
+            return View();
+        }
+
+        //Проектирование и организация проводных локальных сетей
+        public ActionResult LAN()
+        {
+            return View();
+        }
+
+        //IP-телефония
+        public ActionResult IPtelephony()
+        {
+            return View();
+        }
+
+        //Мониторинг деятельности пользователей
+        public ActionResult UserMonitoring()
+        {
+            return View();
+        }
+
+        //Видеоконференцсвязь
+        public ActionResult Videoconferencing()
+        {
+            return View();
+        }
+
+        //Системы видеонаблюдения
+        public ActionResult Videosurveillance()
+        {
+            return View();
+        }
+
+        //Разработка WEB-сайтов
+        public ActionResult Develop()
+        {
+            return View();
+        }
+
+        //Консультирование, сопровождение
+        public ActionResult Support()
+        {
+            return View();
+        }
+    }
+}
